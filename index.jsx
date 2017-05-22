5d117e9ab01a3d7fbc14744923009e36a9dc0944
HSU Rynki <Gernischt@gmail.com>
2017-05-22 10:21:48 +0800

    change CRLF to LF

--- index.jsx
+++ index.jsx
@@ -2,15 +2,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// (window != window.top ? window.top : window).React = React;
 import jQuery from 'jquery';
 window.jQuery = jQuery;
 window.$ = jQuery;
 
 import UAParser from 'ua-parser-js';
 window.ua = (new UAParser()).getResult();
-ua.firefox = ua.browser.name == 'Firefox';
-ua.edge = ua.browser.name == 'Edge';
+ua.firefox = ua.browser.name === 'Firefox';
+ua.edge = ua.browser.name === 'Edge';
 
 import LazyLoader from 'LazyLoader';
 
